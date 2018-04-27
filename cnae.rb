bf1e2e26a5c15c70c4c3e3e058afc14e56d4ee7d
Igor C. Batista <igor.batista@gmail.com>
2018-04-27 10:26:16 -0300

    script description cnae

--- cnae.rb
+++ cnae.rb
@@ -3,7 +3,7 @@ require 'json'
 
 module Cnae
   
-  def self.banks
+  def self.cnae
     http = Net::HTTP.new('raw.githubusercontent.com', 443); http.use_ssl = true
     JSON.parse http.get('/igorcb/cnae/master/cnae.json').body
   end
@@ -11,10 +11,10 @@ module Cnae
   def self.populate
     cnae.each do |cnae|
       cnae = Atividade.create_with(cnae: cnae["Código CNAE 2.1"], 
-      														 descricao_cnae: cnae["Descrição do Código CNAE 2.0"]
-      														 item_lista: cnae["Item da Lista"]
-      														 descricao_item_lista: cnae["Descrição do Item da Lista (LC Nº 116/2003)"]
-      														 ).find_or_create_by(nome: cnae["Código CNAE 2.1"])
+      														 descricao_cnae: cnae["Descrição do Código CNAE 2.0"],
+      														 item_lista: cnae["Item da Lista"],
+      														 descricao_item_lista: cnae["Descrição do Item da Lista (LC Nº 116/2003)"],
+      														 ).find_or_create_by(cnae: cnae["Código CNAE 2.1"])
     end
   end
 end
